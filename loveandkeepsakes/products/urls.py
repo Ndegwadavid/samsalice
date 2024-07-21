ad35f90173b93f1a2e8cafe9a96127bc82ac5150
Ndegwadavid <davidndegwa6869@gmail.com>
2024-07-21 14:08:29 +0300

    Development: deleted unnecessary app

--- loveandkeepsakes/products/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.product_list, name='product_list'),
-    path('<str:category>/', views.category_products, name='category_products'),
-]
